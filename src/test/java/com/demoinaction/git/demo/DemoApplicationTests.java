87c8b68363e8082d47dae4e6ee1d36633b3db89c
demoinaction <demoinaction@outlook.com>
2020-06-14 16:16:13 +0800

    add to localrepository

---
+++ src/test/java/com/demoinaction/git/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoinaction.git.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
